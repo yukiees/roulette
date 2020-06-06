a4b3280e636e2cb524c71274cee86c25eccd2f8f
yukiees <yukiii8989@gmail.com>
2020-06-06 21:52:34 +0900

    remove setButton

--- roulette/ViewController.swift
+++ roulette/ViewController.swift
@@ -11,25 +11,15 @@ import UIKit
 var listData: [String] = []
 
 class ViewController: UIViewController {
-    @IBOutlet weak var test1: UILabel!
-    @IBOutlet weak var test2: UILabel!
-    @IBOutlet weak var test3: UILabel!
-    @IBOutlet weak var test4: UILabel!
-    @IBOutlet weak var test5: UILabel!
-    @IBOutlet weak var test6: UILabel!
-    @IBOutlet weak var test7: UILabel!
-    @IBOutlet weak var test8: UILabel!
     @IBOutlet weak var rouletteView: UIImageView!
     @IBOutlet weak var rouletteButton: UIButton!
     @IBOutlet weak var selectList: UIButton!
-    @IBOutlet weak var rouletteSetButton: UIButton!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         rouletteButton.layer.cornerRadius = 10
-        rouletteSetButton.layer.cornerRadius = 10
         selectList.layer.cornerRadius = 10
         rouletteView.layer.cornerRadius = 150
     
@@ -54,34 +44,19 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func onTappedSetButton(){
-        if (listData != []){
-            test1.text = listData[0]
-            test2.text = listData[1]
-            test3.text = listData[2]
-            test4.text = listData[3]
-            test5.text = listData[4]
-            test6.text = listData[5]
-            test7.text = listData[6]
-            test8.text = listData[7]
-            
-        }
-    }
+
     
      func roulette() {
         
         let number = Int.random(in: 0..<360)
         
         UIView.animate(withDuration: 5, animations: {
-            for _ in 0...5 {
+            for _ in 0...3{
+            
             self.rouletteView.transform = CGAffineTransform(rotationAngle: CGFloat.pi/180*180)
             self.rouletteView.transform = CGAffineTransform(rotationAngle: CGFloat.pi/180*360)
+        }
             self.rouletteView.transform = CGAffineTransform(rotationAngle: CGFloat.pi/180*CGFloat(number))
-            
-                self.test1.transform = CGAffineTransform(rotationAngle: CGFloat.pi/180*180)
-                self.test1.transform = CGAffineTransform(rotationAngle: CGFloat.pi/180*360)
-                self.test1.transform = CGAffineTransform(rotationAngle: CGFloat.pi/180*CGFloat(number))
-            }
     })
     }
 }
